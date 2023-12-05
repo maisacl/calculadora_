8b86bd9fdcf68c13c4aa91d4a99de6a62c2c644a
maisa <maysaclara290@gmail.com>
2023-12-05 10:22:37 -0300

    primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+input('Vamos calcular!')
+
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+
+resultado = numero1 + numero2 
+
+print(resultado)
